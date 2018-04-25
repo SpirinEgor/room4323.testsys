70c07821e9a309343e85708918692ce903a4e313
voudy <spirin.egor@mail.ru>
2018-04-26 01:26:52 +0300

    clean code

--- backend/testsys-backend/src/main/kotlin/testsys_backend/Application.kt
+++ backend/testsys-backend/src/main/kotlin/testsys_backend/Application.kt
@@ -1,14 +1,18 @@
 package testsys_backend
+
 import com.google.gson.GsonBuilder
-import io.ktor.application.*
-import io.ktor.features.*
+import io.ktor.application.Application
+import io.ktor.application.call
+import io.ktor.application.install
+import io.ktor.features.CallLogging
+import io.ktor.features.ContentNegotiation
+import io.ktor.features.DefaultHeaders
 import io.ktor.gson.gson
-import io.ktor.http.*
-import io.ktor.response.*
-import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
-import java.io.File
+import io.ktor.http.HttpStatusCode
+import io.ktor.response.respond
+import io.ktor.routing.get
+import io.ktor.routing.route
+import io.ktor.routing.routing
 
 fun Application.main() {
     install(DefaultHeaders)
@@ -18,54 +22,51 @@ fun Application.main() {
             setPrettyPrinting()
         }
     }
-    routing{
-        route("/api"){
-            get("tasks"){
+
+    routing {
+        route("/api") {
+            get("tasks") {
                 val fileContent = this.javaClass.getResource("/tasks.json").readText()
                 val gson = GsonBuilder().create()
                 val tasks: Tasks = gson.fromJson(fileContent, Tasks::class.java)
 
                 call.respond(tasks)
             }
-            get("tasks/{id}"){
+            get("tasks/{id}") {
                 val fileContent = this.javaClass.getResource("/tasks.json").readText()
                 val gson = GsonBuilder().create()
                 val tasks: Tasks = gson.fromJson(fileContent, Tasks::class.java)
                 //try catch
                 val id = call.parameters["id"]?.toInt()
-                if(id == null)
-                    call.respond {HttpStatusCode.ExceptionFailed}
-                else{
+                if (id == null) {
+                    call.respond { HttpStatusCode.ExceptionFailed }
+                } else {
                     val item = tasks.getTask(id)
-                    if(item == null)
+                    if (item == null) {
                         call.respond(HttpStatusCode.NotFound)
-                    else
+                    } else {
                         call.respond(item)
-
+                    }
                 }
             }
-            get("getStatus/{id}"){
+            get("getStatus/{id}") {
                 val fileContent = this.javaClass.getResource("/status.json").readText()
                 val gson = GsonBuilder().create()
                 val status: Status = gson.fromJson(fileContent, Status::class.java)
-                //try catch
+                // try catch
                 call.respond(status)
                 val id = call.parameters["id"]?.toInt()
-
-                if(id == null)
-                    call.respond {HttpStatusCode.ExceptionFailed}
-                else{
+                if (id == null) {
+                    call.respond { HttpStatusCode.ExceptionFailed }
+                } else {
                     val item = status.getLastSubmission()
-                    if(item == null)
+                    if (item == null) {
                         call.respond(HttpStatusCode.NotFound)
-                    else
+                    } else {
                         call.respond(item)
-
+                    }
                 }
-
             }
         }
-
     }
 }
-
