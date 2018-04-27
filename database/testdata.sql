d3d17cfe8b581930e9a0fb697546303cd1044947
Alexander Markov <admarkov@lenta.ru>
2018-04-27 10:58:07 +0300

    database things

---
+++ database/testdata.sql
@@ -0,0 +1,21 @@
+INSERT INTO Problems (name) VALUES ('A + B');
+INSERT INTO Problems (name) VALUES ('K-pop');
+INSERT INTO Problems (name) VALUES ('Задача с русским названием');
+INSERT INTO Problems (name) VALUES ('Задача A');
+INSERT INTO Problems (name) VALUES ('Задача B');
+
+INSERT INTO Users (username, fullname) VALUES ('admarkov', 'Alexander Markov');
+INSERT INTO Users (username, fullname) VALUES ('nadmarkov', 'Александр Марков');
+
+INSERT INTO Submits (pr_id, user_id) VALUES(1, 1);
+UPDATE Submits SET status = 'finished', docker_return = 'array from docker', verdict = 'OK' where id = 1;
+INSERT INTO Submits (pr_id, user_id) VALUES(1, 2);
+UPDATE Submits SET status = 'finished', docker_return = 'array from docker', verdict = 'TL', testid = 12, comment="" where id = 2;
+INSERT INTO Submits (pr_id, user_id) VALUES(2, 1);
+UPDATE Submits SET status = 'finished', docker_return = 'array from docker', verdict = 'WA', testid = 5, comment="expected 'a', found 'b'" where id = 3;
+INSERT INTO Submits (pr_id, user_id) VALUES(3, 1);
+UPDATE Submits SET status = 'running' where id = 4;
+INSERT INTO Submits (pr_id, user_id) VALUES(2, 2);
+UPDATE Submits SET status = 'running' where id = 5;
+INSERT INTO Submits (pr_id, user_id) VALUES(4, 1);
+INSERT INTO Submits (pr_id, user_id) VALUES(5, 1);
--- database/testsys.sql
+++ database/testsys.sql
@@ -1,10 +1,10 @@
 CREATE TABLE Problems (
-    id serial primary key,
+    id integer primary key autoincrement,
     name text not null
 );
 
 CREATE TABLE Tags (
-    id serial primary key,
+    id integer primary key autoincrement,
     tag text not null
 );
 
@@ -14,13 +14,13 @@ CREATE TABLE problems_tags (
 );
 
 CREATE TABLE Contests (
-    id serial primary key,
+    id integer primary key autoincrement,
     name text not null,
     start_at timestamptz
 );
 
 CREATE TABLE Users (
-    id serial primary key,
+    id integer primary key autoincrement,
     username varchar(20) not null,
     fullname text not null
 );
@@ -37,10 +37,20 @@ CREATE TABLE problems_contests (
 );
 
 CREATE TABLE Submits (
-    id serial primary key,
+    id integer primary key autoincrement,
     pr_id integer references Problems(id),
     user_id integer references Users(id),
     status varchar(20) default 'pending',
-    time timestamp default CURRENT_TIMESTAMP,
-    verdict text
+    submtime timestamp default CURRENT_TIMESTAMP,
+    docker_return text,
+    verdict varchar(2),
+    testid integer default -1,
+    comment text
 );
+
+CREATE VIEW ExtSubmits AS
+    SELECT submits.id as submid, problems.id as prid, problems.name as problem_name, users.id as uid, users.username as uname,
+     users.fullname as fullname, submits.status, submits.submtime, submits.docker_return, submits.verdict, submits.testid, submits.comment
+    FROM Submits
+    JOIN Users on Submits.user_id = Users.id
+    JOIN Problems on Submits.pr_id = Problems.id;
