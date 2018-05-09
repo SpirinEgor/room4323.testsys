1a0f8b8f2b59a39ae8df87be70fdf254db280497
Alexander Markov <admarkov@lenta.ru>
2018-05-09 22:42:17 +0300

    Minor changes of template requests

--- database/Submits.sql
+++ database/Submits.sql
@@ -8,3 +8,7 @@ UPDATE Submits SET status = 'running' where id = <id сабмита>;
 UPDATE Submits SET status = 'finished', docker_return = <возврат докера>, verdict = 'OK' where id = <id сабмита>;
 --Докер дотестировал, задача не получила ОК
 UPDATE Submits SET status = 'finished', docker_return = <возврат докера>, verdict = <'OK'|'WA'|'TL'|...>, testid = <номер теста, на котором программа упала>, comment=<комментарий чекера> where id = <id сабмита>;
+--Получить все сабмиты пользователя
+SELECT * from ExtSubmits where uid = <id пользователя>
+--Получить все сабмиты пользователя по задаче
+SELECT * from Submits where user_id = <id пользователя> and pr_id = <id задачи>
